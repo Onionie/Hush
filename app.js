c054aef0e44796d0b45d65caaa60061379db9a75
Onionie <gutierrez.onionie@gmail.com>
2021-09-22 23:23:58 -0700

    Added Database to store users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const port = 3000;
 
 const app = express();
@@ -10,6 +11,28 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//connect to mongodb using mongoose
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+
+//create new Schema
+const userShema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userShema);
+
+app.get("/", function (req, res){
+  res.render("home")
+});
+
+app.get("/login", function (req, res){
+  res.render("login")
+});
+
+app.get("/register", function (req, res){
+  res.render("register")
+});
 
 
 app.listen(port, () => {

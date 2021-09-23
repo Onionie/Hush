7eb4b3549ee8d14048030db8b139534214d8caa2
Onionie <gutierrez.onionie@gmail.com>
2021-09-22 23:41:58 -0700

    Successfully login level 1

--- app.js
+++ app.js
@@ -34,9 +34,11 @@ app.get("/register", function (req, res){
   res.render("register")
 });
 
+//////////// POST methods ///////////////
+
 app.post("/register", function(req, res){
   const newUser = new User({
-    email: req.body.email,
+    email: req.body.username,
     password: req.body.password
   });
 
@@ -48,6 +50,25 @@ app.post("/register", function(req, res){
       res.render("secrets")
     }
   });
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUser){
+        if(foundUser.password = password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+
 });
 
 

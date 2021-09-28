a44577757f1ad58452af993724d36f4ea2a3c97a
Onionie <gutierrez.onionie@gmail.com>
2021-09-28 15:32:21 -0700

    Fixed user Serialization

--- app.js
+++ app.js
@@ -46,8 +46,15 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
@@ -71,6 +78,13 @@ app.get("/auth/google", passport.authenticate("google", {
     scope: ["profile"]
 
 }));
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/login"}),
+  function(req, res) {
+    // Successful authentication, redirect secrets page.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function (req, res){
   res.render("login")
